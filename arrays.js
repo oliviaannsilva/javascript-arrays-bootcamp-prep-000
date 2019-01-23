152414629598b6f2d9c78694098ee5653ddc6008
Olivia Silva <olivia.silva@wework.com>
2019-01-23 22:10:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -36,7 +36,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array,element){
   return array
 }
 
-function removeElementFromBeginningOfArray(array,element){
+function removeElementFromBeginningOfArray(array){
   array.slice(1)
   return array
 }
