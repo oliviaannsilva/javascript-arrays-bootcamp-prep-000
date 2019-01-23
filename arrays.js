379e43e3090fc6018e990485b1bca4f74ed2fc5f
Olivia Silva <olivia.silva@wework.com>
2019-01-23 22:22:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -46,4 +46,8 @@ function destructivelyRemoveElementFromEndOfArray(array,element){
   return array
 }
 
-function 
+function removeElementFromEndOfArray(array){
+  array.slice(0,
+  array.length-1)
+  return array
+}
